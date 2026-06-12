95d7829d23bdacb9bbd48023bd5c2543c416dfcf
lyndsey isoe <lyndsey.nyanduko512@gmail.com>
2026-06-12 10:24:59 +0300

    Complete OOp lab

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,22 @@
-#!/usr/bin/env python3
-
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self._size = None
+        self.size = size  # triggers setter
+        self.price = price
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        allowed = ["Small", "Medium", "Large"]
+
+        if value in allowed:
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
